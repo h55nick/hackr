6fdbf581f5af30564366cf9e17a01070918661e5
Blanchet <h55nick@gmail.com>
2013-03-13 17:53:45 -0400

    initial commit (yml ignore, gemfile,test_helper)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackr::Application.config.secret_token = '3e8c51074c78c37f71eb0d43680eec941c3032a65662972c7c1d4470d3d4f7898bd58bdf8e926fa6b79baad8c7600bbc660dd51f547f55e372e3edef480d907a'
